f224174623d75b5d74f723c18376a8f04940a45d
Manshi Rathour <manshi2086@gmail.com>
2026-06-11 12:31:02 +0530

    tools calling and execution

--- 16_17_Tools/clear_metadata.py
+++ 16_17_Tools/clear_metadata.py
@@ -1,9 +1,13 @@
 import nbformat
 
-nb = nbformat.read("tools_in_langchain.ipynb", as_version=4)
+# nb = nbformat.read("tools_in_langchain.ipynb", as_version=4)
+# nb = nbformat.read("tool_calling_basics.ipynb", as_version=4)
+nb = nbformat.read("Currency_Conversion.ipynb", as_version=4)
 
 if "widgets" in nb.metadata:
     del nb.metadata["widgets"]
 
-nbformat.write(nb, "tools_in_langchain.ipynb")
+# nbformat.write(nb, "tools_in_langchain.ipynb")
+# nbformat.write(nb, "tool_calling_basics.ipynb")
+nbformat.write(nb, "Currency_Conversion.ipynb")
 print("done")
